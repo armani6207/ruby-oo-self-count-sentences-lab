63bdb3f94cab1e83b35e735fa41e579c87c5f57d
Armando <rojas.armani@gmail.com>
2020-11-20 12:43:36 -0500

    commit tree 1ec2cbbbe7f5919514f09918aae3f327aa33b66a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = 0
+    self.split.map { |word| sentences += 1 if ['.', '?', '!'].include?(word[-1]) }
+    sentences
   end
 end
